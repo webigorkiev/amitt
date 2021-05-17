fc8c642e5a89f70124048fb08a796b45cde1f005
Igor Khomenko <webigorkiev@gmail.com>
2021-05-17 20:14:07 +0300

    fix some settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,6 +43,8 @@ module.exports = {
         "no-return-assign": "off",
         "no-sequences": "off",
         "no-shadow": "off",
-        "@typescript-eslint/no-var-requires": "off"
+        "@typescript-eslint/no-var-requires": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/ban-ts-comment": "off"
     },
 };
--- readme.md
+++ readme.md
@@ -2,6 +2,6 @@
 
 ### **Clone node-ts-starter-package**
 
-`git clone --depth=1 ssh://tehnoskarb@35.158.77.103:/var/www/tehnoskarb/git/@jwn-js/node-ts-starter-package.git .`
+`git clone --depth=1 ssh://tehnoskarb@godisonline.net:/var/www/tehnoskarb/git/@jwn-js/node-ts-starter-package.git .`
 
 Then delete **.git** dir
