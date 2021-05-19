91d96f111382f7940674fe12b943e5d0728765e9
Igor Khomenko <webigorkiev@gmail.com>
2021-05-19 19:11:53 +0300

    next version typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -45,6 +45,7 @@ module.exports = {
         "no-shadow": "off",
         "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/no-explicit-any": "off",
-        "@typescript-eslint/ban-ts-comment": "off"
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off"
     },
 };
